6d5653a97425a4a57581e5cbb1f703fef4a51eb2
Samuel Offem <samoffem@gmail.com>
2020-05-07 10:49:12 +0100

    redux store

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,11 @@
+const initState = [
+    {id: 1, title: "Hadoop and its inconsistencies", content: "blah blah blah"},
+    {id: 2, title: "Big data analytics Manipulation", content: "blah blah blah"},
+    {id: 3, title: "Javascript arrays and methods", content: "blah blah blah"}
+]
+
+const projectReducer = (state = initState, action)=>{
+    return state
+}
+
+export default projectReducer
